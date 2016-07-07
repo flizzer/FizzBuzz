32ceada34a829a3a6cedb85b6d5291cbd67372ef
flizzer <bdavis@macpro.local>
2016-07-06 22:50:28 -0400

    adding more tests and logic for doing the math needed by the game

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -4,11 +4,39 @@ class Brain: NSObject {
    
     func IsDivisibleByThree(number: Int) -> Bool
     {
-        let result = number % 3
+        return IsDivisibleBy(3, number: number)
+    }
+    
+    func IsDivisibleByFive(number: Int) -> Bool
+    {
+        return IsDivisibleBy(5, number: number)
+    }
+    
+    func IsDivisibleByFifteen(number: Int) -> Bool
+    {
+        return IsDivisibleBy(15, number: number)
+    }
+    
+    func IsDivisibleBy(divisor: Int, number: Int) -> Bool
+    {
+        let result = number % divisor
         if result == 0 {
             return true
         }
         return false
     }
     
+    func Check(number: Int) -> String
+    {
+        if IsDivisibleByFifteen(number) {
+            return "FizzBuzz"
+        }
+        else if IsDivisibleByThree(number) {
+            return "Fizz"
+        }
+        else if IsDivisibleByFive(number) {
+            return "Buzz"
+        }
+        return "\(number)"
+    }
 }
--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -11,24 +11,65 @@ import XCTest
 
 class BrainTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
+    let brain = Brain()
     
     func testIsDivisibleByThree() {
-        let brain = Brain()
+        
         let result = brain.IsDivisibleByThree(3)
         XCTAssertEqual(result, true)
     }
     
-    func testIsNotDivisibleByThrree() {
-        let brain = Brain()
+    func testIsNotDivisibleByThree() {
         let result = brain.IsDivisibleByThree(1)
         XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFive()
+    {
+        let result = brain.IsDivisibleByFive(5)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByFive()
+    {
+        let result = brain.IsDivisibleByFive(1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFifteen()
+    {
+        let result = brain.IsDivisibleByFifteen(15)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByFifteen()
+    {
+        let result = brain.IsDivisibleByFifteen(1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testSayFizz()
+    {
+        let result = brain.Check(3)
+        XCTAssertEqual(result, "Fizz")
+    }
+    
+    func testSayBuzz()
+    {
+        let result = brain.Check(5)
+        XCTAssertEqual(result, "Buzz")
+    }
+    
+    func testSayFizzBuzz()
+    {
+        let result = brain.Check(15)
+        XCTAssertEqual(result, "FizzBuzz")
+    }
+    
+    func testCheckForOne()
+    {
+        let result = brain.Check(1)
+        XCTAssertEqual(result, "1")
     }
     
 }
